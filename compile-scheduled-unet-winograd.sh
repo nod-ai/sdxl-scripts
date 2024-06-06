3c94f92db8b7dbbd0543c463e02617473588154b
Jakub Kuderski <jakub@nod-labs.com>
2024-06-06 18:55:42 +0000

    Fix dump dir name

--- compile-scheduled-unet-winograd.sh
+++ compile-scheduled-unet-winograd.sh
@@ -21,10 +21,10 @@ set -x
 "${SCRIPT_DIR}/compile-unet-base.sh" "$IREE_COMPILE" "$CHIP" winograd \
   "${SCRIPT_DIR}/specs/${TRANSFORM_PREFIX}attention_and_matmul_spec.mlir" \
   "${SCRIPT_DIR}/base_ir/stable_diffusion_xl_base_1_0_PNDM_64_1024x1024_fp16_unet_30.mlir" \
-  --iree-hal-dump-executable-configurations-to=configurations/scheduled_unet \
-  --iree-hal-dump-executable-sources-to=sources/scheduled_unet \
-  --iree-hal-dump-executable-binaries-to=binaries/scheduled_unet \
-  --iree-hal-dump-executable-benchmarks-to=benchmarks/scheduled_unet \
+  --iree-hal-dump-executable-configurations-to=configurations/scheduled_unet_winograd \
+  --iree-hal-dump-executable-sources-to=sources/scheduled_unet_winograd \
+  --iree-hal-dump-executable-binaries-to=binaries/scheduled_unet_winograd \
+  --iree-hal-dump-executable-benchmarks-to=benchmarks/scheduled_unet_winograd \
   -o "${PWD}/tmp/scheduled_unet.vmfb" \
   "$@"
 
