9c364246e7f679ac9b218e4c3a3ccc06fa9fda8d
Eric Eaton <Eric.Eaton@amd.com>
2024-06-11 11:49:13 -0500

    Spoof iree-benchmark-module better
    This should make the power_trace.sh wrapper work more broadly.

--- power_trace.sh
+++ power_trace.sh
@@ -2,13 +2,28 @@
 
 set -xe
 
+tmpdir=$(mktemp -d -t "traceXXXXXX")
+
+cleanup () {
+    rm -r $tmpdir
+}
+trap cleanup EXIT
+
 script_dir=$(dirname $(realpath $0))
 
 real_iree=$(command -v iree-benchmark-module)
 
-iree-benchmark-module () {
-  sudo LD_LIBRARY_PATH=/usr/local/lib runTracer.sh $real_iree $@
-  python3 $script_dir/corellator.py trace.rpd
-}
+
+cat << EOF > $tmpdir/iree-benchmark-module
+#!/bin/bash
+sudo LD_LIBRARY_PATH=/usr/local/lib runTracer.sh $real_iree \$@
+ret=$?
+python3 $script_dir/corellator.py trace.rpd || echo "Returned $?"
+exit $ret
+EOF
+
+chmod a+x $tmpdir/iree-benchmark-module
+
+export PATH=$tmpdir:$PATH
 
 source $@
