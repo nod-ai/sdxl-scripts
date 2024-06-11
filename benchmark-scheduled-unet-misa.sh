24dc7d2a4af067aeabb6a088bac63b8bb7f973e4
amdsc21 <nithinsubbiah@gmail.com>
2024-06-11 04:01:11 +0000

    sanitize bash script

--- benchmark-scheduled-unet-misa.sh
+++ benchmark-scheduled-unet-misa.sh
@@ -4,17 +4,12 @@
 
 set -xeu
 
-if (( $# != 1 && $# != 2 )); then
-  echo "usage: $0 <hip-device-id> [<ipra-path-prefix>]"
-  exit 1
-fi
-
 IRPA_PATH_PREFIX="${2:-/data/shark}"
 
 iree-benchmark-module \
   --device=rocm://$1 \
   --device_allocator=caching \
-  --module=$PWD/tmp/scheduled_unet_misa.vmfb \
+  --module="$PWD"/tmp/scheduled_unet_misa.vmfb \
   --parameters=model=${IRPA_PATH_PREFIX}/scheduled_unet.irpa \
   --function=run_forward \
   --input=1x4x128x128xf16 \
