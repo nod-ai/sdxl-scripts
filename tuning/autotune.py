452ba93cafa81dfcb5e2485068170374ed1f9a19
RattataKing <46631728+RattataKing@users.noreply.github.com>
2024-06-11 22:21:24 -0400

    Update tuning/autotune.py
    Co-authored-by: Jakub Kuderski <jakub@nod-labs.com>

--- tuning/autotune.py
+++ tuning/autotune.py
@@ -250,7 +250,6 @@ def numerical_sort_key(path):
 
 
 def calculate_md5(file_path):
-    """calculate the MD5 hash"""
     md5 = hashlib.md5()
     with open(file_path, 'rb') as f:
         for chunk in iter(lambda: f.read(4096), b''):
