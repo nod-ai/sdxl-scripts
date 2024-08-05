fa34a1bc6fc48d597162d02b0ed73566a37024be
Mihaescu Vlad <52869843+mihaescuvlad@users.noreply.github.com>
2024-08-05 23:29:00 +0300

    [tuner] Add device validation for user devices (#60)
    The input will be verified against the available devices on the user's
    machine.
    
    The old implementation of `--devices` was taking in an integer, it will
    now take in a string representing the device's Path / UUID.
    
    * Adds: `fetch_available_devices() -> list[any]` - Returns the list of
    all of the devices found by the IREE Runtime module
    * Adds: `validate_devices(user_devices) -> None` - Takes in the user
    input and checks it against the available devices and throws an error if
    at least one of the user's devices is invalid.
    * Modified: `parse_devices(devices_str: str) -> list[str]` - The
    documentation of the method has been updated and `validate_devices` will
    be called to validate the user input
    * Modified: `parse_arguments()` - The help message has been updated to
    better represent what the new input format for `--devices` is expected
    to look like
    * Fixed typo: `Bnechmarking` -> `Benchmarking`

--- tuning/benchmark_unet_candidate.sh
+++ tuning/benchmark_unet_candidate.sh
@@ -9,7 +9,7 @@ shift 2
 echo "Benchmarking: ${INPUT} on device ${DEVICE}"
 
 timeout 180s tools/iree-benchmark-module \
-  --device="hip://${DEVICE}" \
+  --device="${DEVICE}" \
   --hip_use_streams=true \
   --hip_allow_inline_execution=true \
   --device_allocator=caching \
