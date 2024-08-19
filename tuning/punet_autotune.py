a1f68e6f6fba6a543f7cb7433b36c6561dea7ade
Amily Wu <amilywu2@amd.com>
2024-08-19 10:15:34 -0500

    Add amd header. Remove unwanted import

--- tuning/punet_autotune.py
+++ tuning/punet_autotune.py
@@ -1,5 +1,10 @@
+# Copyright 2024 Advanced Micro Devices, Inc
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
 import autotune
-import subprocess
 from dataclasses import dataclass
 from pathlib import Path
 
