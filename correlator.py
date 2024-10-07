24fa7296fb7cb630a8d40de8a29a1a3c31b94fd9
Eric Eaton <Eric.Eaton@amd.com>
2024-10-07 18:26:59 -0500

    Refactor corellator script

--- correlator.py
+++ correlator.py
@@ -8,7 +8,7 @@ import json
 import sys
 import itertools
 import argparse
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 @dataclass
 class Interval:
@@ -105,8 +105,61 @@ def load_json(path, plot, stretch_plot, skew):
     
     return reg_values, interval_list, stretch_values
 
+def mean(items, _):
+    return sum(items)/len(items)
 
-def load_rpd(path, plot, stretch_plot, freq, skew):
+@dataclass
+class Metric:
+    name: str
+    trace_name: str
+    top: bool
+    dtype: type
+    cumulative: bool = False
+    values: dict = field(default_factory=dict)
+    kern_values: dict = field(default_factory=dict)
+    no_kern: int = 0
+    total = 0
+    no_kern_sum = 0
+
+    def load_intervals(self, intervals):
+        for ts in self.values:
+            interval = find_interval(ts, intervals)
+            if interval is None:
+                self.no_kern += 1
+                if self.cumulative:
+                    self.no_kern_sum += self.values[ts]
+            else:
+                if not interval.kernel in self.kern_values:
+                    self.kern_values[interval.kernel] = []
+                self.kern_values[interval.kernel].append(self.values[ts])
+
+    def summary(self):
+        result = f'{self.name} ({self.trace_name}): {len(self.values)} Samples, '
+        if self.cumulative:
+            result += f' Total: {self.total} Samples w/o kernel: {self.no_kern}'
+        else:
+            result += f' Average: {self.total/len(self.values)}'
+        return result
+
+    def report(self, note, count, rank, percent=False):
+        value_ranks = {}
+        for kern in self.kern_values:
+            value_ranks[kern] = rank(self.kern_values[kern], kern)
+
+        sorted_rank = sorted(value_ranks.items(), key=lambda x: x[1], reverse=self.top)
+        
+        count = min(count, len(sorted_rank))
+
+        print('Top' if self.top else 'Bottom', count, f'kernels ({self.name})', note)
+        for kernel, rank in sorted_rank[:count]:
+            if percent:
+                print(kernel, rank, '{:.3f}%'.format(rank/self.total*100.0))
+            else:
+                print(kernel, rank)
+
+        return sorted_rank
+
+def load_rpd(path, metrics, skew):
     print("Loading file", path)
     con = sqlite3.connect(path)
     cur = con.cursor()
@@ -121,23 +174,18 @@ def load_rpd(path, plot, stretch_plot, freq, skew):
     for start, end, name in kerns:
         interval_list.append(Interval(name, start, end))
 
-    plots = cur.execute(f"SELECT start, value FROM rocpd_monitor WHERE monitorType = '{plot}'").fetchall()
-    for time, value in plots:
-        value = int(value)
-        reg_values[time + skew] = value
-    
-    plots = cur.execute(f"SELECT start, value FROM rocpd_monitor WHERE monitorType = '{stretch_plot}'").fetchall()
-    for time, value in plots:
-        value = float(value)
-        stretch_values[time + skew] = value
+    for metric in metrics:
+        plots = cur.execute(f"SELECT start, value FROM rocpd_monitor WHERE monitorType = '{metric.trace_name}'").fetchall()
+        for time, value in plots:
+            value = metric.dtype(value)
+            metric.values[time + skew] = value
 
-    plots = cur.execute(f"SELECT start, value FROM rocpd_monitor WHERE monitorType = '{freq}'").fetchall()
-    for time, value in plots:
-        value = float(value)
-        freq_values[time + skew] = value
-    print("freq", len(plots))
+    return interval_list
 
-    return reg_values, interval_list, stretch_values, freq_values
+def prepare_metrics(metrics, intervals):
+    for metric in metrics:
+        metric.total = sum(metric.values.values())
+        metric.load_intervals(intervals)
 
 def add_arguments(parser: argparse.ArgumentParser):
     parser.add_argument('--plots', default='/etc/correlator.json', help="The plot name that counts events")
@@ -152,8 +200,12 @@ def main(argv):
 
     with open(args.plots, 'r') as fp:
         plots = json.load(fp)
-    event_plot = plots['counter']
-    stretch_plot = plots['stretch']
+
+    pcc_events = Metric("PCC Events", plots['counter'], True, int, True)
+    clock_stretch = Metric("Clock Stretch", plots['stretch'], True, float)
+    gfx_frequency = Metric("GFX Frequency", plots['freq'], False, float)
+
+    metrics = [pcc_events, clock_stretch, gfx_frequency]
 
     if args.input.endswith('json'):
         loader = load_json
@@ -162,101 +214,38 @@ def main(argv):
     else:
         print("Unknown input format")
         return 1
-    reg_values, interval_list, stretch_values, freq_values = loader(args.input, event_plot, stretch_plot, plots['freq'], args.skew)
-    
-    reg_total = sum(reg_values.values())
+    interval_list = loader(args.input, metrics, args.skew)
 
-    kernel_data = {}
-    kernel_time = {}
-    kernel_stretch = {}
-    freq_data = {}
-
-    for interval in interval_list:
-        name = interval.kernel
-        if not name in kernel_data:
-            kernel_data[name] = []
-            freq_data[name] = []
-        kernel_time[name] = kernel_time.get(name, 0.0) + (interval.end - interval.start)
-
-    print('Num data points', event_plot, len(reg_values))
-    print('Num data points', stretch_plot, len(stretch_values))
-    print('Num kernels:', len(kernel_data))
-    print('Num intervals:', len(interval_list))
-    print('Total', event_plot, reg_total)
-
-    if len(reg_values) == 0 or len(kernel_data) == 0:
-        print('There is a problem with the trace, it is missing critical data.')
-        return 1
-
+    print('Assemble btree')
     interval_btree = Node(None)
     insert_intervals(interval_list, interval_btree, True)
 
-    print('Btree assembled')
+    print('Prepare Metrics')
+    prepare_metrics(metrics, interval_btree)
 
-    no_kern = 0
-    no_kern_sum = 0
+    kernel_time = {}
 
-    for ts in reg_values:
-        interval = find_interval(ts, interval_btree)
-        if interval is None:
-            no_kern += 1
-            no_kern_sum += reg_values[ts]
-        else:
-            kernel_data[interval.kernel].append(reg_values[ts])
+    for interval in interval_list:
+        name = interval.kernel
+        kernel_time[name] = kernel_time.get(name, 0.0) + (interval.end - interval.start)
 
-    stretch_times = sorted(stretch_values.keys())
-    for i in range(1, len(stretch_times)):
-        growth = stretch_values[stretch_times[i]] - stretch_values[stretch_times[i-1]]
-        if growth > 0:
-            interval = find_interval(stretch_times[i], interval_btree)
-            if interval is not None:
-                kernel_stretch[interval.kernel] = kernel_stretch.get(interval.kernel, 0.0) + growth
+    print('Num kernels:', len(kernel_time))
+    print('Num intervals:', len(interval_list))
 
-    print('Data points without kernel:', no_kern, "({} {:.3f}%)".format(no_kern_sum, no_kern_sum / reg_total * 100))
-    print('Data points with kernel:', len(reg_values) - no_kern, "({} {:.3f}%)".format(reg_total - no_kern_sum, (reg_total - no_kern_sum) / reg_total * 100))
+    for metric in metrics:
+        print(metric.summary())
 
-    kernel_sums = {}
-    for kern in kernel_data:
-        kernel_sums[kern] = sum(kernel_data[kern])
+    if any([len(x.values) == 0 for x in metrics]):
+        print('There is a problem with the trace, it is missing critical data.')
+        return 1
 
-    sorted_sums = sorted(kernel_sums.items(), key=lambda x: x[1], reverse=True)
+    def event_rate(values, kern):
+        return 1000.0 * sum(values) / kernel_time[kern] if kernel_time[kern] > 0 else 0.0
 
-    top_n = min(args.n, len(sorted_sums))
-    print('Top', top_n, 'offenders (total events)')
-    for kernel, ksum in sorted_sums[:top_n]:
-        print(kernel, ksum, '{:.3f}%'.format(ksum/reg_total*100.0))
-
-    sorted_rate = sorted([(x[0], x[1] / kernel_time[x[0]] if kernel_time[x[0]] > 0 else 0.0)
-                         for x in kernel_sums.items()], key=lambda x: x[1], reverse=True)
-
-    print('Top', top_n, 'offenders (events/us)')
-    for kernel, rate in sorted_rate[:top_n]:
-        print(kernel, '{:.3f}'.format(rate * 1000.0))
-
-    sorted_growth = sorted(kernel_stretch.items(), key=lambda x: x[1], reverse=True)
-    print('Top', top_n, 'stretch growth')
-    for kernel, growth in sorted_growth[:top_n]:
-        print(kernel, '{:.3f}'.format(growth))
-
-    for ts in freq_values:
-        interval = find_interval(ts, interval_btree)
-        if interval is None:
-            pass
-        else:
-            freq_data[interval.kernel].append(freq_values[ts])
-
-    freq_avg = {}
-    for kern in freq_data:
-        data = freq_data[kern]
-        if len(data) > 0:
-            avg = sum(data)/len(data)
-            freq_avg[kern] = avg
-
-
-    print('Bottom', top_n, 'average frequency')
-    sorted_freq = sorted(freq_avg.items(), key=lambda x: x[1])
-    for kernel, freq in sorted_freq[:top_n]:
-        print(kernel, '{:.0f}'.format(freq/1000.0))
+    pcc_events.report('Count', args.n, lambda x, _: sum(x), True)
+    pcc_events.report('Rate (per us)', args.n, event_rate)
+    clock_stretch.report('Average', args.n, mean)
+    gfx_frequency.report('Average', args.n, mean)
 
     return 0
 
