49e08700a274a7f25e35c87c1c930b022a7ad7cf
Nithin Meganathan <18070964+nithinsubbiah@users.noreply.github.com>
2025-01-09 11:50:05 -0800

    Update benchmark-unet.sh

--- fp16-model/benchmark-unet.sh
+++ fp16-model/benchmark-unet.sh
@@ -4,21 +4,21 @@
 
 set -xeu
 
-# if (( $# != 1 && $# != 2 )); then
-#   echo "usage: $0 <hip-device-id> [<ipra-path-prefix>]"
-#   exit 1
-# fi
+if (( $# != 1 && $# != 2 )); then
+  echo "usage: $0 <hip-device-id> [<ipra-path-prefix>]"
+  exit 1
+fi
 
-# IRPA_PATH_PREFIX="${2:-/data/shark}"
+IRPA_PATH_PREFIX="${2:-/data/shark}"
 
 # used as a workaround for lengthy initialization
 # export ROCR_VISIBLE_DEVICES=0
 
 iree-benchmark-module \
-  --device=hip \
+  --device=hip://$1 \
   --device_allocator=caching \
   --module=$PWD/tmp/unet.vmfb \
-  --parameters=model=/home/nmeganat/SHARK-ModelDev/weights/stable_diffusion_xl_base_1_0_unet_fp16.safetensors \
+  --parameters=model=${IRPA_PATH_PREFIX}/scheduled_unet.irpa \
   --function=run_forward \
   --input=@sample_inputs/unet_npys/arg0_latent_model_input.npy \
   --input=@sample_inputs/unet_npys/arg1_guidanc_scale.npy \
