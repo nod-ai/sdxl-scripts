630fb6c6aaee8cb542b79d6ed983c4217beed57b
Rahul Kayaith <rkayaith@gmail.com>
2025-02-20 13:40:57 -0600

    Update vae and punet compile flags

--- int8-model/compile-vae.sh
+++ int8-model/compile-vae.sh
@@ -1,46 +1,65 @@
 #!/bin/bash
 
-# Usage: PATH=/path/to/iree/build/tools:$PATH ./compile-vae.sh [extra flags]
+# Usage: PATH=/path/to/iree/build/tools:$PATH ./compile-vae.sh <target-chip> <tuning-chip-configuration-mode> <batch-size>
 
 set -xeu
 
 readonly SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)"
 
-if (( $# != 2 )); then
-  echo "usage: $0 <target-chip> <batch-size>"
+if (( $# != 3 )); then
+  echo "usage: $0 <target-chip> <tuning-chip-configuration-mode> <batch-size>"
   exit 1
 fi
 
+WORKING_DIR=${WORKING_DIR:-${SCRIPT_DIR}}
+
+rm -rf "${WORKING_DIR}/configurations/vae"
+rm -rf "${WORKING_DIR}/sources/vae"
+rm -rf "${WORKING_DIR}/binaries/vae"
+rm -rf "${WORKING_DIR}/benchmarks/vae"
+
 CHIP=$1;       shift
+CHIP_CONFIGURATION=$1; shift
 BATCH_SIZE=$1; shift
 
+if ! [[ "${CHIP_CONFIGURATION}" =~ ^(none|cpx|qpx)$ ]]; then
+  echo "Allowed tuning-chip-configuration-modes: none, cpx, qpx"
+  exit 1
+fi
+
+ATTENTION_SPEC="${SCRIPT_DIR}/specs/attention_and_matmul_spec_vae_mi300_${CHIP_CONFIGURATION}.mlir"
+if [ ! -f "$ATTENTION_SPEC" ] ; then
+  echo "Specified attention spec file not found: ${ATTENTION_SPEC}"
+  exit 1
+fi
+
 readonly PREPROCESSING_FLAGS=(
-"--iree-preprocessing-pass-pipeline=builtin.module(util.func(iree-global-opt-raise-special-ops, iree-flow-canonicalize), iree-preprocessing-transpose-convolution-pipeline, util.func(iree-preprocessing-pad-to-intrinsics), util.func(iree-preprocessing-generalize-linalg-matmul-experimental))"
+"--iree-preprocessing-pass-pipeline=builtin.module(util.func(iree-global-opt-raise-special-ops, iree-flow-canonicalize), iree-preprocessing-transpose-convolution-pipeline, iree-preprocessing-pad-to-intrinsics, util.func(iree-preprocessing-generalize-linalg-matmul-experimental), iree-preprocessing-convert-conv-filter-to-channels-last{filter-layout=fhwc})"
 )
 declare -a FLAGS=("${PREPROCESSING_FLAGS[*]}")
 
-iree-compile ${SCRIPT_DIR}/base_ir/stable_diffusion_xl_base_1_0_bs1_1024x1024_fp16_vae_decomp_attn.mlir \
+iree-compile ${SCRIPT_DIR}/base_ir/20250220/stable_diffusion_xl_base_1_0_vae_bs1_1024x1024_fp16.mlir \
+    --iree-config-add-tuner-attributes \
     --iree-hal-target-backends=rocm \
     --iree-hip-target=${CHIP} \
-    --iree-rocm-bc-dir="${SCRIPT_DIR}/../bitcode-6.1.2" \
+    --iree-hip-bc-dir="${SCRIPT_DIR}/../bitcode-6.1.2" \
     --iree-vm-bytecode-module-output-format=flatbuffer-binary \
-    --iree-opt-const-eval=false \
-    --iree-opt-data-tiling=false \
-    --iree-global-opt-propagate-transposes=true \
-    --iree-opt-aggressively-propagate-transposes=true \
-    --iree-dispatch-creation-enable-fuse-horizontal-contractions=false \
-    --iree-dispatch-creation-enable-aggressive-fusion \
-    --iree-vm-target-truncate-unsupported-floats \
-    --iree-codegen-llvmgpu-use-vector-distribution \
-    --iree-llvmgpu-enable-prefetch \
     --iree-codegen-gpu-native-math-precision=true \
-    --iree-execution-model=async-external \
-    --iree-codegen-transform-dialect-library="${SCRIPT_DIR}/specs/attention_and_matmul_spec.mlir" \
-    --iree-hal-dump-executable-configurations-to=configurations/vae \
-    --iree-hal-dump-executable-sources-to=sources/vae \
-    --iree-hal-dump-executable-binaries-to=binaries/vae \
-    --iree-hal-dump-executable-benchmarks-to=benchmarks/vae \
-    -o $PWD/tmp/vae_decode.vmfb \
+    --iree-codegen-llvmgpu-early-tile-and-fuse-matmul=true \
+    --iree-dispatch-creation-enable-aggressive-fusion \
+    --iree-dispatch-creation-enable-fuse-horizontal-contractions=false \
+    --iree-hal-indirect-command-buffers=true \
+    --iree-hal-memoization=true \
+    --iree-llvmgpu-enable-prefetch \
+    --iree-opt-outer-dim-concat=true \
+    --iree-opt-strip-assertions \
+    --iree-stream-resource-memory-model=discrete \
+    --iree-codegen-transform-dialect-library=$ATTENTION_SPEC \
+    --iree-hal-dump-executable-configurations-to="${WORKING_DIR}/configurations/vae" \
+    --iree-hal-dump-executable-sources-to="${WORKING_DIR}/sources/vae" \
+    --iree-hal-dump-executable-binaries-to="${WORKING_DIR}/binaries/vae" \
+    --iree-hal-dump-executable-benchmarks-to="${WORKING_DIR}/benchmarks/vae" \
+    -o $WORKING_DIR/tmp/vae_decode.vmfb \
     "${FLAGS[@]}" \
     "$@"
     #--iree-opt-outer-dim-concat=true \
