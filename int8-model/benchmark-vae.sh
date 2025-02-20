db55af7f7bef2d3435a3292f3b55b4ebd03d804b
Rahul Kayaith <rkayaith@gmail.com>
2025-02-20 15:03:26 -0600

    Add BS1 input data for VAE

--- int8-model/benchmark-vae.sh
+++ int8-model/benchmark-vae.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Usage: PATH=/path/to/iree/build/tools:$PATH ./benchmark-vae.sh N
+# IRPA file can be found at https://sharkpublic.blob.core.windows.net/sharkpublic/sdxl/weights/stable_diffusion_xl_base_1_0_vae_dataset_fp16.irpa
 
 set -xeu
 
@@ -8,6 +9,7 @@ if (( $# != 2 && $# != 3 )); then
   echo "usage: $0 <hip-device-id> <batch-size> [<ipra-path-prefix>]"
   exit 1
 fi
+readonly SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)"
 
 DEVICE=$1;     shift
 BATCH_SIZE=$1; shift
@@ -19,11 +21,13 @@ if ! [[ $BATCH_SIZE =~ $re ]] ; then
    exit 1
 fi
 
+INPUT_PATH="${SCRIPT_DIR}/vae_npys"
+
 iree-benchmark-module \
   --device=hip://${DEVICE} \
   --device_allocator=caching \
   --module=$PWD/tmp/vae_decode.vmfb \
-  --parameters=model=${IRPA_PATH_PREFIX}/vae_decode_fp16.irpa \
+  --parameters=model=${IRPA_PATH_PREFIX}/stable_diffusion_xl_base_1_0_vae_dataset_fp16.irpa \
   --function=decode \
-  --input=${BATCH_SIZE}x4x128x128xf16 \
+  --input=@${INPUT_PATH}/vae_inputs_bs${BATCH_SIZE}/latents.npy \
   --benchmark_repetitions=3
