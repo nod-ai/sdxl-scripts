9196dc5d236677e1bb540185ff89febf695fa0b0
Andrea Faulds <andrea.faulds@amd.com>
2025-03-05 10:37:29 -0800

    Revert int8 model to old version that did not use fp8

--- int8-model/benchmark-unet.sh
+++ int8-model/benchmark-unet.sh
@@ -4,34 +4,25 @@
 
 set -xeu
 
-if (( $# != 2 && $# != 3 )); then
-  echo "usage: $0 <hip-device-id> <batch-size> [<ipra-path-prefix>]"
+if (( $# != 1 && $# != 2 )); then
+  echo "usage: $0 <hip-device-id> [<ipra-path-prefix>]"
   exit 1
 fi
 
-readonly SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)"
-readonly IREE_BENCHMARK="$(which iree-benchmark-module)"
-readonly HIP_DEVICE="$1"
-readonly BATCH_SIZE="$2"
-if ! [[ "${BATCH_SIZE}" =~ ^(1|4|8|14)$ ]]; then
-  echo "Allowed batch-sizes: 1, 4, 8, 14"
-  exit 1
-fi
-INPUT_PATH="${SCRIPT_DIR}/unet_npys"
-INPUTS="--input=@${INPUT_PATH}/unet_inputs_bs${BATCH_SIZE}/latent_model_input.npy \
---input=@${INPUT_PATH}/unet_inputs_bs${BATCH_SIZE}/guidance_scale.npy \
---input=@${INPUT_PATH}/unet_inputs_bs${BATCH_SIZE}/prompt_embeds.npy \
---input=@${INPUT_PATH}/unet_inputs_bs${BATCH_SIZE}/add_text_embeds.npy \
---input=@${INPUT_PATH}/unet_inputs_bs${BATCH_SIZE}/add_time_ids.npy \
---input=@${INPUT_PATH}/unet_inputs_bs${BATCH_SIZE}/t.npy"
+IRPA_PATH_PREFIX="${2:-/data/shark}"
 
-IRPA_PATH_PREFIX="${3:-/data/shark}"
-
-"$IREE_BENCHMARK" \
-  --device="hip://${HIP_DEVICE}" \
+iree-benchmark-module \
+  --device=hip://$1 \
+  --hip_use_streams=true \
+  --hip_allow_inline_execution=true \
   --device_allocator=caching \
-  --module="${SCRIPT_DIR}/tmp/punet_bs${BATCH_SIZE}.vmfb" \
+  --module=$PWD/tmp/punet.vmfb \
   --parameters=model=${IRPA_PATH_PREFIX}/sdxl_unet_int8_dataset.irpa \
   --function=main \
-  $INPUTS \
+  --input=1x4x128x128xf16 \
+  --input=1xsi32 \
+  --input=2x64x2048xf16 \
+  --input=2x1280xf16 \
+  --input=2x6xf16 \
+  --input=1xf16 \
   --benchmark_repetitions=3
